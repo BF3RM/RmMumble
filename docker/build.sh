cc9b28ab90c2c06f5e7a361178cafb4c3528eb8b
Jury Verrigni <jury.verrigni@gmail.com>
2018-05-11 18:10:52 +0100

    Removes g15 emulator

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source /docker/setup-env.sh
-x86_64-w64-mingw32.static-qmake-qt5 -recursive CONFIG+="release g15-emulator no-overlay no-bonjour no-elevation no-ice asio"
+x86_64-w64-mingw32.static-qmake-qt5 -recursive CONFIG+="release no-g15 no-overlay no-bonjour no-elevation no-ice asio"
 make
-cp prhk.bin release/
-zip -r /output/pr-mumble-$GIT_COMMIT.zip release/ -i '*.exe' '*.dll' '*.bin'
+cp prhk.bin release/ && zip -r /output/pr-mumble-$GIT_COMMIT.zip release/ -i '*.exe' '*.dll' '*.bin'

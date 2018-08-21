60378a29a61a793281e15fd07ac1ed97e3f975d3
Jury Verrigni <jury.verrigni@gmail.com>
2018-08-21 17:34:38 +0100

    Adds debug option to mxe build

--- mxe/build.sh
+++ mxe/build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+SHOULD_PACK=false
+BUILD_TYPE=release
+
 pack() {
-    cp prhk.bin release/ && zip -r rm-mumble.zip release/ -i '*.exe' '*.dll' '*.bin'
+    cp prhk.bin ${BUILD_TYPE}/ && zip -r rm-mumble-${BUILD_TYPE}.zip ${BUILD_TYPE}/ -i '*.exe' '*.dll' '*.bin'
 }
+
+while test $# -gt 0
+do
+    case "$1" in
+        -pack) SHOULD_PACK=true
+            ;;
+        -debug) BUILD_TYPE=debug 
+            ;;
+    esac
+    shift
+done
 
 if [ ! -f mxe/build.sh ]; then
     echo "Please run this script from the parent folder (./mxe/build.sh)."
@@ -12,20 +26,11 @@ fi
 export MUMBLE_PROTOC=/usr/lib/mxe/usr/x86_64-unknown-linux-gnu/bin/protoc
 export PATH=$PATH:/usr/lib/mxe/usr/bin
 
-x86_64-w64-mingw32.static-qmake-qt5 -recursive CONFIG+="release no-g15 no-overlay no-bonjour no-elevation no-ice asio" INCLUDEDIR+="/usr/lib/mxe/usr/x86_64-w64-mingw32.static/qt5/include/"
-make -j10
+x86_64-w64-mingw32.static-qmake-qt5 -recursive CONFIG+="${BUILD_TYPE} no-g15 no-overlay no-bonjour no-elevation no-ice asio" INCLUDEDIR+="/usr/lib/mxe/usr/x86_64-w64-mingw32.static/qt5/include/"
+make
 
 EXIT_STATUS=$?
 
-if [ $EXIT_STATUS -eq 0 ]; then
-    while test $# -gt 0
-    do
-        case "$1" in
-            -pack) pack
-                ;;
-            #*) echo "argument $1"
-            #    ;;
-        esac
-        shift
-    done
+if [ $EXIT_STATUS -eq 0 ] && [ $SHOULD_PACK ]; then
+    pack
 fi

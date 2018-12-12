3e36723dd252f79b0bc28dd7d4dd89ee3035e667
StuartSulaski <stuartsulaski@gmail.com>
2018-12-12 21:10:12 +0100

    Changing ACL permissions

--- src/ACL.cpp
+++ src/ACL.cpp
@@ -36,7 +36,7 @@ ChanACL::ChanACL(Channel *chan) : QObject(chan) {
 bool ChanACL::hasPermission(ServerUser *p, Channel *chan, QFlags<Perm> perm, ACLCache *cache) {
 	Permissions granted = effectivePermissions(p, chan, cache);
 	// RM_DEBUG (Platy): Users are only allowed to speak so everything else is false by default
-	if ((perm & Speak) != None)
+	if ((perm & Whisper) != None)
 	{
 		return true;		
 	}

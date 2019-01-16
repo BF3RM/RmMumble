ec01c0864ec7dcc81db7840ca36bb1696f207152
Jury Verrigni <jury.verrigni@gmail.com>
2019-01-16 17:43:10 +0000

    Sticks to mumble's mxe mirror

--- mxe/dependencies.sh
+++ mxe/dependencies.sh
@@ -15,8 +15,8 @@ apt update
 apt install -y gnupg
 apt install -y dirmngr wget zip unzip
 
-echo "deb http://pkg.mxe.cc/repos/apt/debian jessie main" > /etc/apt/sources.list.d/mxeapt.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
+echo "deb https://mumble.info/snapshot/mirror/pkg.mxe.cc/repos/apt/debian jessie main" > /etc/apt/sources.list.d/mxeapt.list
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
 apt update
 
 apt install -y \

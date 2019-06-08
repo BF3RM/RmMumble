3b300238d96e6c8553bb453502a70a7ae504b109
Jury Verrigni <jury.verrigni@gmail.com>
2019-06-08 16:16:32 +0100

    Removes iowin32 from minizip sources on unix

--- cmake/mumble/updater.cmake
+++ cmake/mumble/updater.cmake
@@ -2,6 +2,10 @@ find_package(Qt5 COMPONENTS Network Core Widgets)
 
 file(GLOB MINIZIP_SOURCES ${CMAKE_SOURCE_DIR}/3rdparty/zlib/contrib/minizip/*.c)
 
+if(UNIX)
+    list(REMOVE_ITEM MINIZIP_SOURCES "${CMAKE_SOURCE_DIR}/3rdparty/zlib/contrib/minizip/iowin32.c")
+endif()
+
 add_library(UpdaterLib STATIC ${CMAKE_SOURCE_DIR}/src/updater/RmUpdater.cpp ${MINIZIP_SOURCES})
 target_include_directories(UpdaterLib PUBLIC ${CMAKE_SOURCE_DIR}/src/updater ${CMAKE_SOURCE_DIR}/3rdparty/zlib/ ${CMAKE_BINARY_DIR}/3rdparty/zlib)
 target_link_libraries(UpdaterLib PRIVATE Qt5::Network Qt5::Widgets zlib)
